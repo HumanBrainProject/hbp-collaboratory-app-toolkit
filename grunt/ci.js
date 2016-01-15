e1fffcaab95b5a168e3ca999fe4111b630da5dcd
Olivier Amblet <olivier.amblet@epfl.ch>
2016-01-15 10:26:25 +0100

    release minor
    Change-Id: Ie76ae0f695f78084390d5485e2773c186d217877

--- grunt/ci.js
+++ grunt/ci.js
@@ -12,18 +12,18 @@ module.exports = function(grunt) {
     var done = this.async();
     var exec = require('child_process').exec;
     // Inspect the log message to see if we need to release
-    exec('git log --format=%B --no-merges -n 1', function(error, stdout, stderr){
+    exec('git log --format=%B --no-merges -n 1', function(error, stdout){
       if (error) {
         grunt.log.error('Error checking commit message', error);
       }
-      var matches = stdout.match(/release (patch|minor|major|prerelease|prepatch|preminor|premajor|git)/)
+      var matches = stdout.match(/release (patch|minor|major|prerelease|prepatch|preminor|premajor|git)/);
       if (process.env.CI && matches && matches[1]) {
-        grunt.log.writeln('New release detected: ' + matches[1])
+        grunt.log.writeln('New release detected: ' + matches[1]);
         tasks.unshift('bump:' + matches[1] + ':bump-only');
         tasks.push('exec:changelog', 'exec:gitRelease');
       }
       grunt.task.run(tasks);
-      done()
+      done();
     });
   };
 };
